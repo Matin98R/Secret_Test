99763417326ff7be25e244f006bab34a6c820b08
Matin <matin.haj@bechtle.com>
2026-02-09 12:28:34 +0100

    added resource

--- main.tf
+++ main.tf
@@ -7,3 +7,46 @@ module "avm-res-automation-automationaccount" {
   sku = "Free"
 }
 
+resource "azurerm_resource_group" "example" {
+  name     = "mh_rg_test"
+  location = "West Europe"
+}
+
+resource "azurerm_automation_account" "example" {
+  name                = "example-account"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku_name            = "Free"
+
+  tags = {
+    environment = "Test"
+  }
+}
+
+/*
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_automation_account" "example" {
+  name                = "example-account"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku_name            = "Basic"
+}
+
+resource "azurerm_automation_source_control" "example" {
+  name                  = "example"
+  automation_account_id = azurerm_automation_account.example.id
+  folder_path           = "runbook"
+
+  security {
+    token      = "ghp_xxx"
+    token_type = "PersonalAccessToken"
+  }
+  repository_url      = "https://github.com/foo/bat.git"
+  source_control_type = "GitHub"
+  branch              = "main"
+}
+*/
